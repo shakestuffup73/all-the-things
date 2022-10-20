695cc468bea41391089a2262799bdf15fdfd190f
lilyshowlader <lilyshowlader@gmail.com>
2022-10-20 13:52:29 -0400

    add lilys thing

---
+++ src/pages/LilysThings/LilysThings.jsx
@@ -0,0 +1,15 @@
+import ThingCard from '../../components/ThingCard/ThingCard'
+import { Link } from 'react-router-dom'
+
+const LilysThings = (props) => {
+  return (
+    <>
+      <h1>Lily's Things</h1>
+      <Link to="/">Home</Link>
+
+      <h1>heyyyyyyyyyyyy</h1>
+    </>
+  )
+}
+ 
+export default LilysThings
